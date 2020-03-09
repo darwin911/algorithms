18cbebc61bd459217fba221fd59df86bc3d03495
Darwin Smith <darwin911@gmail.com>
2020-03-09 10:56:27 -0400

    Created. Combines unique elements of given arrays

---
+++ uniteUnique.js
@@ -0,0 +1,12 @@
+// Sorted Union
+// Joins unique elements of given arrays
+
+function uniteUnique(arr) {
+  let result = [];
+  for (let i = 0; i < arguments.length; i++) {
+    result = [...result, ...arguments[i]];
+  }
+  return [...new Set(result)];
+}
+
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
