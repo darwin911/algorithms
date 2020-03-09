1329c982dfea7530b96305711acea5bf22df5a83
Darwin Smith <darwin911@gmail.com>
2020-03-09 11:16:15 -0400

    nominal change

--- missingLetters.js
+++ missingLetters.js
@@ -3,7 +3,7 @@
   ===============
   Find the missing letter in the passed letter range and return it.
   If all letters are present in the range, return undefined.
-  ==================================================================
+  =================================================================
 */
 
 let alphabet = 'abcdefghijklmnopqrstuvwxyz';
