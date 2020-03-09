b5b4eee1ee68b34791138bf9bfb5ad2e91000e7f
Darwin Smith <darwin911@gmail.com>
2020-03-09 15:11:13 -0400

    adds all prime numbers up to n

---
+++ sumPrimes.js
@@ -0,0 +1,28 @@
+/*
+  Sum Primes
+  ==========
+  A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+  Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+  ==========
+*/
+
+const sumPrimes = num => {
+  let result = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) result += i;
+  }
+  return result;
+};
+
+const isPrime = value => {
+  for (let i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+  return value > 1;
+};
+
+// console.log(sumPrimes(10)); // 17
+console.log(sumPrimes(977)); // 73156
